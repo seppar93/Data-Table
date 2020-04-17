861e6098c1c46a739b674b6d0f4d78ce0f50e707
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-04-17 15:14:35 -0400

    redux setup

---
+++ src/components/DataTableComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class DataTableComponent extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DataTableComponent
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducers/rootReducer'
+
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
