ceaa0ca5c556727e6c03a03b7c2d58e5178abd84
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-04-21 15:40:52 -0400

    all data displayed

--- src/components/DataTableComponent.js
+++ src/components/DataTableComponent.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Row, Col, Container, Table } from 'reactstrap';
 
-
 // Services
 import UserAPIServices from '../services/UserAPIServices';
 
@@ -17,21 +16,28 @@ import {
 } from '../redux/actions/action';
 import * as actions from '../redux/actions/action';
 
-
-// 
-const UserInfo = props => {
+//
+const UserInfo = (props) => {
   let thumbNail = props.user.picture.thumbnail;
+  let name = `${props.user.name.title} ${props.user.name.first} ${props.user.name.last} `;
+  let address = `${props.user.location.country}: ${props.user.location.state}: ${props.user.location.city}`;
+  let contact  = `Email: ${props.user.email}  Number: ${props.user.phone}`
   console.log(props);
-  
 
   return (
-      <tr>
-        <th scope='row'>
-          <img src={thumbNail} />
-        </th>
-      </tr>
+    <tr>
+      <th scope='row'></th>
+      <div>
+      <td><img src={thumbNail} /></td>
+
+      </div>
+      <td>{name} </td>
+      <td>{address}</td>
+      <td>{contact}</td>
+
+    </tr>
   );
-}
+};
 
 export class DataTableComponent extends Component {
   constructor(props) {
@@ -39,7 +45,7 @@ export class DataTableComponent extends Component {
     this.userAPIServices = new UserAPIServices();
 
     this.state = {
-      user: {}
+      user: {},
     };
   }
 
@@ -49,7 +55,7 @@ export class DataTableComponent extends Component {
       .getUser()
       .then((res) => {
         this.props.actions.fetchUserSuccess(res.data.results);
-        this.setState({user: res.data.results});
+        this.setState({ user: res.data.results });
       })
       .catch((error) => {
         this.props.actions.fetchUserError(error);
@@ -57,32 +63,30 @@ export class DataTableComponent extends Component {
   }
 
   render() {
-    
     return (
       <div>
-        <Table>
+        <Table hover>
           <thead>
             <tr>
-              <th>Image</th>
+              <th>
+                <i class='fas fa-arrow-down'></i>
+              </th>
+              <th> Image</th>
               <th>User</th>
               <th>Address</th>
               <th>Contact</th>
             </tr>
           </thead>
           <tbody>
-            {this.props.user?
-            Array.from(this.props.user, ([key, value]) => value).map(val => {
-              return (
-                <UserInfo
-                key={val.name.first}
-                  user={val}></UserInfo>
-              );
-            })
-            :
-            <p>LOADING USERS....</p>
-          
-          }
-            
+            {this.props.user ? (
+              Array.from(this.props.user, ([key, value]) => value).map(
+                (val) => {
+                  return <UserInfo key={val.name.first} user={val}></UserInfo>;
+                }
+              )
+            ) : (
+              <>LOADING USERS....</>
+            )}
           </tbody>
         </Table>
       </div>
