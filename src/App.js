c323a5e1341d11ebc37d12ba413b6d15e8bc2a87
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-04-20 21:52:03 -0400

    lazy loading added and refactor app component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
 // Modules
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Styling
 import './App.css';
 
-// Components
-import DataTableComponent from './components/DataTableComponent';
-
 // Services
 import UserAPIServices from './services/UserAPIServices';
 
-// actions
-import {
-  fetchUserPending,
-  fetchUserSuccess,
-  fetchUserError,
-} from './redux/actions/action';
-import * as actions from './redux/actions/action';
+// Components
+// import DataTableComponent from './components/DataTableComponent';
+const DataTableComponent = lazy(() =>
+  import('./components/DataTableComponent')
+);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.userAPIServices = new UserAPIServices();
-    this.fetchUsers = this.fetchUsers.bind(this);
-    this.state = {};
-  }
-  componentDidMount() {}
+export const App = () => {
+  return (
+    <div className='App'>
+      <Suspense fallback={<div>...loading</div>}>
+        <DataTableComponent />
+      </Suspense>
+    </div>
+  );
+};
 
-  fetchUsers() {
-    this.props.actions.fetchUserPending();
-    this.userAPIServices
-      .getUser()
-      .then((res) => {
-        this.props.actions.fetchUserSuccess(res.data.results);
-        console.log(this.props);
-      })
-      .catch((res) => {
-        this.props.actions.fetchUserError();
-      });
-  }
-
-  render() {
-    console.log(this.props);
-
-    return (
-      <div className='App'>
-        <button onClick={this.fetchUsers}>Testing</button>
-        {/* <DataTableComponent test={'test'} /> */}
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    user: state.users,
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators(actions, dispatch) };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/components/DataTableComponent.js
+++ src/components/DataTableComponent.js
@@ -2,53 +2,56 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import { connect } from 'react-redux';
-
+import { bindActionCreators } from 'redux';
 
 // Services
 import UserAPIServices from '../services/UserAPIServices';
+
+// actions
+import {
+  fetchUserPending,
+  fetchUserSuccess,
+  fetchUserError,
+} from '../redux/actions/action';
+import * as actions from '../redux/actions/action';
 
 export class DataTableComponent extends Component {
   constructor(props) {
     super(props);
     this.userAPIServices = new UserAPIServices();
-    this.fetchUserData = this.fetchUserData.bind(this);
 
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  fetchUserData() {
+  componentDidMount() {
+    this.props.actions.fetchUserPending();
     this.userAPIServices
       .getUser()
       .then((res) => {
-        console.log(res);
+        this.props.actions.fetchUserSuccess(res.data.results);
+        console.log(this.props);
       })
-      .catch((res) => {
-        console.log(res);
+      .catch((error) => {
+        this.props.actions.fetchUserError(error);
       });
   }
-  // componentDidMount() {
-  //   this.userAPIServices
-  //     .getUser()
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((res) => {
-  //       console.log(res);
-  //     });
-  // }
 
   render() {
-    
     return (
       <>
-        <Button onClick={this.fetchUserData}>TESTING</Button>
+        <Button>TESTING</Button>
       </>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    user: state.users,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) };
+}
 
-export default DataTableComponent
-
+export default connect(mapStateToProps, mapDispatchToProps)(DataTableComponent);
